0f29b83216450a242046b2b21869be55c7f4305e
tbriggs1 <tombriggs93@protonmail.com>
2021-03-20 21:49:14 +0000

    Changes to MAC Address.

--- MacChanger.py
+++ MacChanger.py
@@ -3,5 +3,6 @@
 # Imports Subprocess module to allow running bash commands
 import subprocess
 
-
+subprocess.call("ifconfig eth0 down", Shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
 
